3ffe8b2ece29572158bb2b2a3c7fa220fd309b52
Daria Simonova <daria_simonova@hotmail.com>
2024-07-11 08:13:21 +0200

    added playlist data from chatGpt

--- src/main/java/com/example/demo/PlaylistController.java
+++ src/main/java/com/example/demo/PlaylistController.java
@@ -1,7 +1,9 @@
 package com.example.demo;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.ai.azure.openai.AzureOpenAiChatModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,15 +14,26 @@ import org.springframework.web.bind.annotation.RestController;
 public class PlaylistController {
 
     private final PlaylistService playlistService;
+    private final AzureOpenAiChatModel azureOpenAiChatModel;
 
     @Autowired
-    public PlaylistController(PlaylistService playlistService) {
+    public PlaylistController(PlaylistService playlistService, AzureOpenAiChatModel azureOpenAiChatModel) {
         this.playlistService = playlistService;
+        this.azureOpenAiChatModel = azureOpenAiChatModel;
     }
 
     @Operation(summary = "Get a playlist", description = "Fetches most popular song by the artist")
     @GetMapping("/playlist/{artistName}")
-    public PlaylistResponse getPlaylist(@PathVariable String artistName) {
-        return playlistService.getPlaylist(artistName);
+    public String getPlaylist(@PathVariable String artistName) throws JsonProcessingException {
+    return playlistService.getPlaylist(artistName);
     }
+
+
+    //@Operation(summary = "Get a playlist with description", description = "Generated the playlist with songs of similar artists")
+    //@GetMapping("/playlist-data/{artistName}")
+    //public PlaylistResponse getPlaylist(@PathVariable String artistName) {
+    //    return playlistService.getPlaylist(artistName);
+    //}
+
+
 }
