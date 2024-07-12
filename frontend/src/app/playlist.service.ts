21546f799ccf494193f83cec180e141f861d40b0
Daria Simonova <daria_simonova@hotmail.com>
2024-07-12 14:34:22 +0200

    fixes

--- frontend/src/app/playlist.service.ts
+++ frontend/src/app/playlist.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from "../environments/environment";
 
 @Injectable({
   providedIn: 'root'
 })
 export class PlaylistService {
-  private apiUrl = 'http://localhost:5173/playlist/'; // Adjust the URL based on your backend API
+  private apiUrl = environment.backendBaseUrl + '/playlist/';
 
   constructor(private http: HttpClient) {}
 
